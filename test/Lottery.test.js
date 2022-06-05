8d758bfe8725cc911e36dcc960be75c0a5437250
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-05 13:19:40 -0300

    test: add contract coverage

---
+++ test/Lottery.test.js
@@ -0,0 +1,116 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let lottery;
+let manager;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  manager = accounts[0];
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: manager, gas: "1000000" });
+});
+
+describe("Lottery Contract", () => {
+  it("should deploy a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it("should allow one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.01", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(players.length, 1);
+    assert.equal(players[0], accounts[0]);
+  });
+
+  it("should allow multiple accounts to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.01", "ether"),
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.01", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(players.length, 2);
+    assert.equal(players[0], accounts[0]);
+    assert.equal(players[1], accounts[1]);
+  });
+
+  it("should revert if ether value is below 0.01", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei("0.001", "ether"),
+      });
+      assert(false);
+    } catch (error) {
+      assert(error);
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: revert You must enter at least 0.01 ether!"
+      );
+    }
+  });
+
+  it("should revert if someone that isn't the manager calls pickWinner", async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei("0.01", "ether"),
+      });
+
+      await lottery.methods.pickWinner().call({
+        from: accounts[1],
+      });
+
+      assert(false);
+    } catch (error) {
+      assert(error);
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: revert Must be the manager to call this function!"
+      );
+    }
+  });
+
+  it("should sends money to the winner and resets the players array", async () => {
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("10", "ether"),
+    });
+
+    await lottery.methods.pickWinner().call({
+      from: accounts[0],
+    });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    
+    const difference = initialBalance - finalBalance;
+
+    assert(difference > web3.utils.toWei("9.8", "ether"));
+  });
+});
