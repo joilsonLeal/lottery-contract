7c123b2627370865d48722848ba46eeb50b5846c
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-05 12:34:08 -0300

    refactor: conditional on enter method

---
+++ contracts/Lottery.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.17;
+
+contract Lottery {
+  address public manager;
+  address[] public players;
+
+  constructor() public {
+    manager = msg.sender;
+  }
+
+  modifier onlyManager() {
+    require(msg.sender == manager, "Must be the manager to call this function!");
+    _;
+  }
+
+  function enter() public payable {
+    require(msg.value >= .01 ether, "You must enter at least 0.01 ether!");
+    players.push(msg.sender);
+  }
+
+  function random() private view returns (uint256) {
+    return
+      uint256(
+        keccak256(
+          abi.encodePacked(
+            block.timestamp,
+            block.difficulty,
+            msg.sender
+          )
+        )
+      );
+  }
+
+  function pickWinner() public onlyManager() {
+    uint index = random() % players.length;
+    players[index].transfer(address(this).balance);
+    players = new address[](0);
+  }
+
+  function getPlayers() public view returns (address[]) {
+    return players;
+  }
+}
