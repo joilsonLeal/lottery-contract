f45ed030210b4e9e3595ae74e19302f2e239502e
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-05 10:12:36 -0300

    chore: add boilerplate files

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mnemonic:
+    "speed west supreme youth valley fiction alert inspire copper border insane gold",
+  infuraEndpoint:
+    "https://rinkeby.infura.io/v3/fb96b19bfe4d4cb4bee068cbb6ca4e3c"
+};
