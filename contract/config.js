1684ad984424b96eeb4b1a0ba8fd8ac5000fddd5
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-05 17:51:30 -0300

    chore: add web3 and deploy contract

--- contract/config.js
+++ contract/config.js
@@ -2,5 +2,6 @@ module.exports = {
   mnemonic:
     "speed west supreme youth valley fiction alert inspire copper border insane gold",
   infuraEndpoint:
-    "https://rinkeby.infura.io/v3/fb96b19bfe4d4cb4bee068cbb6ca4e3c"
+    "https://rinkeby.infura.io/v3/fb96b19bfe4d4cb4bee068cbb6ca4e3c",
+  contract: "0x47066b63B20dEaA9Dcf626d4041aabE72FFe24f1",
 };
